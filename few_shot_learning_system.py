rewrote all high-end code - still have gradient bug

few_shot_learning_system.py
@@ -45,7 +45,7 @@ class MAMLFewShotClassifier(nn.Module):
 
         self.rng = set_torch_seed(seed=args.seed)
         if args.high_end:
-            self.embedding = HighEndEmbedding(device, args).to(device)
+            self.embedding = HighEndEmbedding(device, args, 3).to(device)
             self.classifier = HighEndClassifier(device, args, self.embedding.n_out_channels).to(device)
         else:
             self.classifier = VGGReLUNormNetwork(im_shape=self.im_shape, num_output_classes=self.args.
@@ -198,8 +198,8 @@ class MAMLFewShotClassifier(nn.Module):
 
             # Inner loop starts
             if self.args.high_end:
-                x_support_set_task = self.embedding(x_support_set_task)
-                x_target_set_task = self.embedding(x_target_set_task)
+                x_support_set_task = self.embedding(x_support_set_task, 0, training=training_phase)
+                x_target_set_task = self.embedding(x_target_set_task, 0, training=training_phase)
             for num_step in range(num_steps):
 
                 # operates on the support set
@@ -290,7 +290,6 @@ class MAMLFewShotClassifier(nn.Module):
         :param num_step: An integer indicating the number of the step in the inner loop.
         :return: the crossentropy losses with respect to the given y, the predictions of the base model.
         """
-        print(weights)
         preds = self.classifier.forward(x=x, params=weights,
                                         training=training,
                                         backup_running_statistics=backup_running_statistics, num_step=num_step)

high_end_maml.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
+import math
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -7,6 +6,8 @@ from meta_neural_network_architectures import *
 
 
 def filter_dict(key, params_dict):
+    if params_dict is None:
+        return None
     res_dict = dict()
     for name, param in params_dict.items():
         bits = name.split('.')
@@ -16,65 +17,125 @@ def filter_dict(key, params_dict):
 
 
 class BatchNorm(nn.Module):
-    def __init__(self, num_features, device, args, eps=1e-5, momentum=0.1, use_per_step_bn_statistics=None):
+    def __init__(self, num_features, device, args, eps=1e-5, momentum=0.1, affine=True,
+                 track_running_stats=True, meta_batch_norm=True, no_learnable_params=False,
+                 use_per_step_bn_statistics=False):
         super(BatchNorm, self).__init__()
         self.eps = eps
+        self.device = device
         self.momentum = momentum
         self.weight = nn.Parameter(torch.ones(num_features))
         self.bias = nn.Parameter(torch.zeros(num_features))
-        # self.register_buffer('running_mean', torch.zeros(num_features))  # This way produced backprop problems
-        # self.register_buffer('running_var', torch.ones(num_features))
         self.running_mean = nn.Parameter(torch.zeros(num_features), requires_grad=False)
-        self.running_var = nn.Parameter(torch.ones(num_features), requires_grad=False)
-        
-    def forward(self, x, num_step):
-        if self.training:
+        self.running_var = nn.Parameter(torch.zeros(num_features), requires_grad=False)
+
+
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        if params:
+            weight = params['weight']
+            bias = params['bias']
+        else:
+            weight = self.weight
+            bias = self.bias
+
+        if training:
             m1 = x.mean((0, 2, 3))
-            m2 = (x**2).mean((0, 2, 3))
+            m2 = (x ** 2).mean((0, 2, 3))
             self.running_mean.data = (1 - self.momentum) * self.running_mean + self.momentum * m1
-            self.running_var.data = (1 - self.momentum) * self.running_var + self.momentum * (m2 - m1**2)
-        return F.batch_norm(x, self.running_mean, self.running_var, self.weight, self.bias, False,
+            self.running_var.data = (1 - self.momentum) * self.running_var + self.momentum * (m2 - m1 ** 2)
+        return F.batch_norm(x, self.running_mean, self.running_var, weight, bias, False,
                             self.momentum, self.eps)
 
 
-# In[2]:
+class Conv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding=0):
+        super(Conv2d, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.weight = nn.Parameter(torch.Tensor(out_channels, in_channels, *kernel_size))
+        self.bias = nn.Parameter(torch.Tensor(out_channels))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        if params:
+            weight = params['weight']
+            bias = params['bias']
+        else:
+            weight = self.weight
+            bias = self.bias
+
+        return F.conv2d(x, weight, bias=bias, padding=self.padding)
 
 
 class BottleneckLayer(nn.Module):
     def __init__(self, in_channels, device, args, batch_norm_cls):
         super(BottleneckLayer, self).__init__()
         self.k = 64  # growth rate
-        self.norm_layer_1 = batch_norm_cls(in_channels,
-                                           device,
-                                           args,
-                                           use_per_step_bn_statistics=args.per_step_bn_statistics)
-        self.conv1 = nn.Conv2d(in_channels, 4 * self.k, 1)
-        self.norm_layer_2 = batch_norm_cls(4 * self.k,
-                                           device,
-                                           args,
-                                           use_per_step_bn_statistics=args.per_step_bn_statistics)
-        self.conv2 = nn.Conv2d(4 * self.k, self.k, 3, padding=1)
-    
-    def forward(self, x, num_step):
-        x = self.conv1(F.relu(self.norm_layer_1(x, num_step)))
-        return self.conv2(F.relu(self.norm_layer_2(x, num_step)))
+        self.bn1 = batch_norm_cls(in_channels, device, args)
+        self.conv1 = Conv2d(in_channels, 4 * self.k, (1, 1))
+        self.bn2 = batch_norm_cls(4 * self.k, device, args)
+        self.conv2 = Conv2d(4 * self.k, self.k, (3, 3), padding=1)
 
-
-# In[3]:
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        x = self.bn1(x, num_step, params=filter_dict('bn1', params), training=training,
+                     backup_running_statistics=backup_running_statistics)
+        x = F.relu(x)
+        x = self.conv1(x, num_step, params=filter_dict('conv1', params), training=training)
+        x = self.bn2(x, num_step, params=filter_dict('bn2', params), training=training,
+                     backup_running_statistics=backup_running_statistics)
+        x = F.relu(x)
+        x = self.conv2(x, num_step, params=filter_dict('conv2', params), training=training)
+        return x
 
 
 class SqueezeExciteConvLayer(nn.Module):
     def __init__(self, in_channels):
         super(SqueezeExciteConvLayer, self).__init__()
         reduced = max(in_channels // 16, 1)
-        self.w1 = nn.Linear(in_channels, reduced, bias=False)
-        self.w2 = nn.Linear(reduced, in_channels, bias=False)
-        
-    def forward(self, x):
+        self.w1 = nn.Parameter(torch.Tensor(reduced, in_channels))
+        self.w2 = nn.Parameter(torch.Tensor(in_channels, reduced))
+
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        if params:
+            w1 = params['w1']
+            w2 = params['w2']
+        else:
+            w1 = self.w1
+            w2 = self.w2
         z = x.mean((-2, -1))
-        z = F.relu(self.w1(z))
-        s = torch.sigmoid(self.w2(z)).unsqueeze(2).unsqueeze(3)
+        F.linear(z, w1)
+        z = F.relu(F.linear(z, w1))
+        s = torch.sigmoid(F.linear(z, w2)).unsqueeze(2).unsqueeze(3)
         return s * x
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, in_channels, device, args, batch_norm_cls):
+        super(DenseBlock, self).__init__()
+        self.se1 = SqueezeExciteConvLayer(in_channels)
+        self.bc1 = BottleneckLayer(in_channels, device, args, batch_norm_cls)
+        self.se2 = SqueezeExciteConvLayer(self.bc1.k + in_channels)
+        self.bc2 = BottleneckLayer(self.bc1.k + in_channels, device, args, batch_norm_cls)
+        self.n_out_channels = self.bc2.k + self.bc1.k + in_channels
+
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        y = self.se1(x, num_step, params=filter_dict('se1', params), training=training)
+        y = self.bc1(x, num_step, params=filter_dict('bc1', params), training=training,
+                     backup_running_statistics=backup_running_statistics)
+        x = torch.cat((x, y), 1)
+        y = self.se2(x, num_step, params=filter_dict('se2', params), training=training)
+        y = self.bc2(x, num_step, params=filter_dict('bc2', params), training=training,
+                     backup_running_statistics=backup_running_statistics)
+        return torch.cat((x, y), 1)
 
 
 class DenseBlockUnit(nn.Module):
@@ -83,47 +144,86 @@ class DenseBlockUnit(nn.Module):
         self.se = SqueezeExciteConvLayer(in_channels)
         self.bc = BottleneckLayer(in_channels, device, args, batch_norm_cls)
         self.n_out_channels = self.bc.k + in_channels
-    
-    def forward(self, x, params, num_step):
-        se_params = filter_dict('se', params)
-        bc_params = filter_dict('bc', params)
-        a = self.se(x, )
-        y = self.bc(self.se(x), num_step)
+
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        y = self.se(x, num_step, params=filter_dict('se', params), training=training)
+        y = self.bc(x, num_step, params=filter_dict('bc', params), training=training,
+                    backup_running_statistics=backup_running_statistics)
         return torch.cat((x, y), 1)
 
 
-# In[29]:
-
 class HighEndEmbedding(nn.Module):
-    def __init__(self, device, args):
+    def __init__(self, device, args, in_channels=3):
         super(HighEndEmbedding, self).__init__()
-        
+
         self.dbu1 = DenseBlockUnit(3, device, args, BatchNorm)
         self.dbu2 = DenseBlockUnit(self.dbu1.n_out_channels, device, args, BatchNorm)
-        
+
         n_out2 = max(self.dbu2.n_out_channels // 2, 1)
-        self.tr_conv = nn.Conv2d(self.dbu2.n_out_channels, n_out2, 1)
+        self.tr_conv = Conv2d(self.dbu2.n_out_channels, n_out2, (1, 1))
         self.tr_av_pool = nn.AvgPool2d(2, stride=2)
-        
+
         self.dbu3 = DenseBlockUnit(n_out2, device, args, BatchNorm)
         self.n_out_channels = self.dbu3.n_out_channels
-    
-    def forward(self, x):
-        x = self.dbu2(self.dbu1(x, 0), 0) # first dense block
-        x = self.tr_av_pool(self.tr_conv(x)) # transition layer
-        return self.dbu3(x, 0)
+
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        # First two dense blocks
+        x = self.dbu1(x, num_step, params=filter_dict('dbu1', params), training=training,
+                      backup_running_statistics=backup_running_statistics)
+        x = self.dbu2(x, num_step, params=filter_dict('dbu2', params), training=training,
+                      backup_running_statistics=backup_running_statistics)
+
+        # Transition
+        x = self.tr_conv(x, num_step, params=filter_dict('tr_conv', params), training=training)
+        x = self.tr_av_pool(x)  #
+
+        # 3/4:th dense block (embedding)
+        x = self.dbu3(x, num_step, params=filter_dict('dbu3', params), training=training,
+                      backup_running_statistics=backup_running_statistics)
+        return x
 
 
 class HighEndClassifier(nn.Module):
     def __init__(self, device, args, in_channels):
         super(HighEndClassifier, self).__init__()
-        
         self.dbu4 = DenseBlockUnit(in_channels, device, args, MetaBatchNormLayer)
-        
-        self.lin1 = nn.Linear(self.dbu4.n_out_channels, args.num_classes_per_set)
-    
-    def forward(self, x, num_step, params, training, backup_running_statistics):
-        x = self.dbu4(x, num_step, params=filter_dict('dbu4', params)).mean((-2, -1))
-        return F.softmax(self.lin1(x), dim=-1)
+        self.weight = nn.Parameter(torch.Tensor(args.num_classes_per_set, self.dbu4.n_out_channels))
+        self.bias = nn.Parameter(torch.Tensor(args.num_classes_per_set))
+        self.reset_parameters()
 
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, x, num_step, params=None, training=False, backup_running_statistics=False):
+        x = self.dbu4(x, num_step, params=filter_dict('dbu4', params), training=training,
+                      backup_running_statistics=backup_running_statistics).mean((-2, -1))
+        if params:
+            weight = params['weight']
+            bias = params['bias']
+        else:
+            weight = self.weight
+            bias = self.bias
+        x = F.linear(x, weight, bias)
+        return F.softmax(x, dim=-1)
+
+    def zero_grad(self, params=None):
+        if params is None:
+            for param in self.parameters():
+                if param.requires_grad == True:
+                    if param.grad is not None:
+                        if torch.sum(param.grad) > 0:
+                            print(param.grad)
+                            param.grad.zero_()
+        else:
+            for name, param in params.items():
+                if param.requires_grad == True:
+                    if param.grad is not None:
+                        if torch.sum(param.grad) > 0:
+                            print(param.grad)
+                            param.grad.zero_()
+                            params[name].grad = None
 